cd17a3fdf720e3735c8f7d5004319872933c5ba5
Juri <juri.strumpflohner@gmail.com>
2021-06-15 21:46:07 +0200

    enable Tailwind CSS purging

--- apps/site/tailwind.config.js
+++ apps/site/tailwind.config.js
@@ -1,6 +1,12 @@
+const { createGlobPatternsOfDependentProjects } = require('@nrwl/next');
+
 module.exports = {
   presets: [require('../../tailwind-workspace-preset.js')],
-  purge: [],
+  purge: [
+    './apps/site/pages/**/*.{js,ts,jsx,tsx}',
+    './apps/site/components/**/*.{js,ts,jsx,tsx}',
+    ...createGlobPatternsOfDependentProjects('site'),
+  ],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
