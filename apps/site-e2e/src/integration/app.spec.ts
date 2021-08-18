1683b2bfd02f39a654e83608ba6149a43bba802f
Juri <juri.strumpflohner@gmail.com>
2021-08-18 21:43:39 +0200

    add e2e test for next app

--- apps/site-e2e/src/integration/app.spec.ts
+++ apps/site-e2e/src/integration/app.spec.ts
@@ -1,13 +1,14 @@
-import { getGreeting } from '../support/app.po';
-
 describe('site', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => {
+    // navigate to an example article
+    cy.visit('/articles/dynamic-routing');
+  });
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('should render the title of the article', () => {
+    cy.get('h1').should('contain', 'Dynamic Routing and Static Generation');
+  });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to site!');
+  it('should properly render the Youtube component', () => {
+    cy.get('iframe').should('be.visible');
   });
 });
