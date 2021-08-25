f74e4945328dc11fd9559eae8055d8ff74ce262e
Juri <juri.strumpflohner@gmail.com>
2021-08-25 09:36:52 +0200

    add new article

--- tools/vercel-deploy/vercel-affected-deploy.sh
+++ tools/vercel-deploy/vercel-affected-deploy.sh
@@ -1,11 +1,13 @@
 
 # Name of the app to check. Change this to your application name!
-APP=tuskdesk
+APP=site
 # Determine version of Nx installed
 NX_VERSION=$(node -e "console.log(require('./package.json').devDependencies['@nrwl/workspace'])")
+TS_VERSION=$(node -e "console.log(require('./package.json').devDependencies['typescript'])")
 
 # Install @nrwl/workspace in order to run the affected command
 npm install -D @nrwl/workspace@$NX_VERSION --prefer-offline
+npm install -D typescript@$TS_VERSION --prefer-offline
 
 # Run the affected command, comparing latest commit to the one before that
 npx nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
