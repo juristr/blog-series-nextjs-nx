6ed107834f87b0397ebadce32ec5ec4ec5a0d005
BBruington <Psychological_Chemist@hotmail.com>
2022-09-25 18:08:01 -0400

    topic-button

--- libs/shared/ui/src/lib/topic-button/topic-button.tsx
+++ libs/shared/ui/src/lib/topic-button/topic-button.tsx
@@ -1,12 +1,45 @@
 import './topic-button.module.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-/* eslint-disable-next-line */
-export interface TopicButtonProps {}
+export interface TopicButtonProps {
+  topicName: string;
+  onClick?: (topicName: string) => void;
+}
 
 export function TopicButton(props: TopicButtonProps) {
+  const [icon, setIcon] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const cleanedSvgName = props.topicName
+        .replace(/[^a-zA-Z0-9]/g, '')
+        .toLocaleLowerCase();
+      const topicSvgIcon = await import(`./${cleanedSvgName}.svg`);
+      setIcon(topicSvgIcon.default);
+    };
+    fetchData();
+  }, [props.topicName]);
+
+  const onClickHandler = () => {
+    if (props.onClick) {
+      props.onClick(props.topicName);
+    } else {
+      console.warn(
+        `no click handler defined on topic button with topic ${props.topicName}`
+      );
+    }
+  };
+
   return (
-    <div>
-      <h1>Welcome to TopicButton!</h1>
+    <div
+      className="bg-white pl-4 rounded-lg shadow flex max-w-md min-w-max hover:shadow-md transition-shadow"
+      onClick={onClickHandler}
+    >
+      <img src={icon} alt="" className="w-12" />
+      <div className="p-5">
+        <h2 className="font-bold text-4xl">{props.topicName}</h2>
+      </div>
     </div>
   );
 }
