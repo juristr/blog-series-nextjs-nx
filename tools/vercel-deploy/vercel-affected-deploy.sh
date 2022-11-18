0b8f4b58a7695255c9946b47e62b2f82bb84e411
Francisco Castillo <fcastillo90@gmail.com>
2022-11-18 13:57:50 +0100

    test-affected

--- tools/vercel-deploy/vercel-affected-deploy.sh
+++ tools/vercel-deploy/vercel-affected-deploy.sh
@@ -6,8 +6,8 @@ NX_VERSION=$(node -e "console.log(require('./package.json').devDependencies['@nr
 TS_VERSION=$(node -e "console.log(require('./package.json').devDependencies['typescript'])")
 
 # Install @nrwl/workspace in order to run the affected command
-npm install -D @nrwl/workspace@$NX_VERSION --prefer-offline
-npm install -D typescript@$TS_VERSION --prefer-offline
+yarn add -D @nrwl/workspace@$NX_VERSION --prefer-offline
+yarn add -D typescript@$TS_VERSION --prefer-offline
 
 # Run the affected command, comparing latest commit to the one before that
 npx nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
